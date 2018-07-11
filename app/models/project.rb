2f3bbf5bfa934a9c2efe10e9770d796c5fdc0aff
Christian N. Kimbi <christiankimbi@gmail.com>
2018-07-12 01:30:52 +0200

    Complete Artifacts

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,8 @@
 class Project < ApplicationRecord
   belongs_to :tenant
+  has_many :artifacts, dependent: :destroy
   validates_uniqueness_of :title
+
   validate :free_plan_can_only_have_one_project
 
 
